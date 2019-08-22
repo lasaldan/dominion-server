ea674fac7d5fb502533b38873f874cbcac194ac6
Daniel Fuller <lasaldan@gmail.com>
2019-08-22 17:16:56 -0600

    Add cors maybe fix

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
 
+// For CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Max-Age', 120);
+    return res.status(200).json({});
+  }
+  next();
+});
+
 server.games = []
 
 var Game = function(name) {
